7e4a63aba54567a407e30f0231d16bd7a9de186c
Kevin Goslar <kevin.goslar@gmail.com>
2022-09-09 07:18:23 -0500

    chore: update Prettier and ory-prettier-style and format everything (#35)

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
-const fs = require('fs')
-const github = require('@actions/github')
-const core = require('@actions/core')
-const queries = require('./queries.js')
-const prettier = require('prettier')
-const oryStyleOptions = require('ory-prettier-styles')
+const fs = require("fs")
+const github = require("@actions/github")
+const core = require("@actions/core")
+const queries = require("./queries.js")
+const prettier = require("prettier")
+const oryStyleOptions = require("ory-prettier-styles")
 
 const ucfirst = (s) => s.charAt(0).toUpperCase() + s.slice(1)
-const token = core.getInput('GITHUB_TOKEN') || process.env.GITHUB_TOKEN
+const token = core.getInput("GITHUB_TOKEN") || process.env.GITHUB_TOKEN
 const octokit = github.getOctokit(token)
 const { repo, owner } = github.context.repo
 
-const outputFile = core.getInput('outputFile') || 'MILESTONES.md'
+const outputFile = core.getInput("outputFile") || "MILESTONES.md"
 const labelMilestonePrefix = core
-  .getInput('labelMilestonePrefix')
+  .getInput("labelMilestonePrefix")
   .toLowerCase()
   .trim()
 
 const orgs = core
-  .getInput('orgs')
-  .split(',')
+  .getInput("orgs")
+  .split(",")
   .map((i) => i.toLowerCase().trim())
   .filter((i) => Boolean(i))
 let ignoreMilestones = core
-  .getInput('ingoreMilestones')
-  .split(',')
+  .getInput("ingoreMilestones")
+  .split(",")
   .map((i) => i.toLowerCase().trim())
   .filter((i) => Boolean(i))
 let onlyLabels = core
-  .getInput('onlyLabels')
-  .split(',')
+  .getInput("onlyLabels")
+  .split(",")
   .map((i) => i.toLowerCase().trim())
   .filter((i) => Boolean(i))
 
 if (onlyLabels.length === 0) {
   onlyLabels = [
-    'bug',
-    'feat',
-    'blocking',
-    'breaking change',
-    'good first issue',
-    'docs',
-    'ci',
-    'rfc',
-    'tests',
-    'upstream',
-    'help wanted'
+    "bug",
+    "feat",
+    "blocking",
+    "breaking change",
+    "good first issue",
+    "docs",
+    "ci",
+    "rfc",
+    "tests",
+    "upstream",
+    "help wanted",
   ]
 }
 
 if (ignoreMilestones.length === 0) {
-  ignoreMilestones = ['unplanned']
+  ignoreMilestones = ["unplanned"]
 }
 
 const renderAssignees = (assignees) =>
   assignees.map(({ name, url }) => `[@${name}](${url})`)
 
-const checkbox = (state) => `[${state.toUpperCase() === 'OPEN' ? ' ' : 'x'}]`
+const checkbox = (state) => `[${state.toUpperCase() === "OPEN" ? " " : "x"}]`
 
 const renderIssuesPulls = (label, issues) =>
   issues
@@ -63,15 +63,15 @@ const renderIssuesPulls = (label, issues) =>
       (issue) =>
         issue.labels
           .map(({ name }) => name.toLowerCase())
-          .indexOf(label.toLowerCase()) > -1
+          .indexOf(label.toLowerCase()) > -1,
     )
     .map(
       ({ title, number, url, assignees, state, repository: { name: repo } }) =>
         `* ${checkbox(state)} ${title} ([${repo}#${number}](${url}))${
           assignees.length === 0
-            ? ''
-            : ` - ${renderAssignees(assignees).join(', ')}`
-        }`
+            ? ""
+            : ` - ${renderAssignees(assignees).join(", ")}`
+        }`,
     )
 
 const renderLabels = ({ issues, labels, pullRequests }) =>
@@ -81,7 +81,7 @@ const renderLabels = ({ issues, labels, pullRequests }) =>
       return self.map(({ name }) => name).indexOf(name) === index
     })
     .map(({ name, description, url }) => {
-      let markdown = ''
+      let markdown = ""
 
       const markdownIssues = renderIssuesPulls(name, issues)
       if (markdownIssues.length > 0) {
@@ -89,7 +89,7 @@ const renderLabels = ({ issues, labels, pullRequests }) =>
 
 #### Issues
 
-${markdownIssues.join('\n')}`
+${markdownIssues.join("\n")}`
       }
 
       const markdownPulls = renderIssuesPulls(name, pullRequests)
@@ -98,7 +98,7 @@ ${markdownIssues.join('\n')}`
 
 #### Pull Requests
 
-${markdownPulls.join('\n')}`
+${markdownPulls.join("\n")}`
       }
 
       if (markdown) {
@@ -121,17 +121,17 @@ ${markdown}`
 const renderMilestones = ({ labels, milestones }) =>
   milestones
     .map(({ title, description, url, dueOn, issues, pullRequests }) => {
-      let markdown = ''
+      let markdown = ""
 
       const markdownLabels = renderLabels({ labels, issues, pullRequests })
       if (markdownLabels) {
-        markdown = markdownLabels.join('\n\n')
+        markdown = markdownLabels.join("\n\n")
       }
 
       if (markdown) {
-        markdown = `## [${title}](${url})${dueOn ? ` - ${dueOn}` : ''}
+        markdown = `## [${title}](${url})${dueOn ? ` - ${dueOn}` : ""}
 
-${description || '*This milestone does not have a description.*'}
+${description || "*This milestone does not have a description.*"}
 
 ${markdown}`
       }
@@ -145,7 +145,7 @@ const canonicalizeIssues = (issues) => (issues || []).map(canonicalizeIssue)
 const canonicalizeIssue = (issue) => ({
   ...issue,
   labels: issue.labels.nodes || [],
-  assignees: issue.assignees.nodes || []
+  assignees: issue.assignees.nodes || [],
 })
 
 const isAllowedMilestone = ({ title }) =>
@@ -160,25 +160,25 @@ const normalizeRepository = (repository) => {
     .map((milestone) => ({
       ...milestone,
       issues: canonicalizeIssues(milestone.issues.nodes),
-      pullRequests: canonicalizeIssues(milestone.pullRequests.nodes)
+      pullRequests: canonicalizeIssues(milestone.pullRequests.nodes),
     }))
 
   return {
     labels,
-    milestones
+    milestones,
   }
 }
 
 const renderRepository = async ({ owner, repo }) => {
   const repository = await fetchRepo(`${owner}/${repo}`)
-  const markdownMilestones = renderMilestones(repository).join('\n\n')
+  const markdownMilestones = renderMilestones(repository).join("\n\n")
 
   return `---
 id: milestones
 title: Milestones and Roadmap
 ---
 
-${markdownMilestones}`.replace(/\n\s*\n/g, '\n\n')
+${markdownMilestones}`.replace(/\n\s*\n/g, "\n\n")
 }
 
 const nameLabel = (name) => `${labelMilestonePrefix}${name}`.toLowerCase()
@@ -188,7 +188,7 @@ const milestoneLabels = ({ milestones }) =>
     .map(({ title }) => `${labelMilestonePrefix}${title}`.toLowerCase())
 
 const fetchRepo = async (name) => {
-  const [owner, repo] = name.split('/')
+  const [owner, repo] = name.split("/")
   const { repository } = await octokit.graphql(queries.project, { repo, owner })
   return normalizeRepository(repository)
 }
@@ -203,8 +203,8 @@ const renderRepositories = async (orgs) => {
     .map(
       async ({ org, label }) =>
         await octokit.graphql(queries.findIssuesPulls, {
-          search: `label:${label} user:${org}`
-        })
+          search: `label:${label} user:${org}`,
+        }),
     )
 
   const results = (await Promise.all(tasks))
@@ -214,21 +214,23 @@ const renderRepositories = async (orgs) => {
       ({ labels, issues, pullRequests }, node) => {
         return {
           issues:
-            node.url.indexOf('/issues/') > -1 ? [...issues, node] : issues,
+            node.url.indexOf("/issues/") > -1 ? [...issues, node] : issues,
           pullRequests:
-            node.url.indexOf('/pull/') > -1
+            node.url.indexOf("/pull/") > -1
               ? [...pullRequests, node]
               : pullRequests,
-          labels: [...labels, ...node.labels]
+          labels: [...labels, ...node.labels],
         }
       },
-      { labels: [], issues: [], pullRequests: [] }
+      { labels: [], issues: [], pullRequests: [] },
     )
 
-  const filterIssue = ({ title }) => ({ labels }) => {
-    const labelNames = labels.map(({ name }) => name.toLowerCase())
-    return labelNames.indexOf(nameLabel(title)) > -1
-  }
+  const filterIssue =
+    ({ title }) =>
+    ({ labels }) => {
+      const labelNames = labels.map(({ name }) => name.toLowerCase())
+      return labelNames.indexOf(nameLabel(title)) > -1
+    }
 
   const document = {
     ...meta,
@@ -237,34 +239,34 @@ const renderRepositories = async (orgs) => {
       ...milestone,
       issues: [
         ...milestone.issues,
-        ...results.issues.filter(filterIssue(milestone))
+        ...results.issues.filter(filterIssue(milestone)),
       ],
       pullRequests: [
         ...milestone.pullRequests,
-        ...results.pullRequests.filter(filterIssue(milestone))
-      ]
-    }))
+        ...results.pullRequests.filter(filterIssue(milestone)),
+      ],
+    })),
   }
 
-  const markdownMilestones = renderMilestones(document).join('\n\n')
+  const markdownMilestones = renderMilestones(document).join("\n\n")
 
   return `---
 id: milestones
 title: Milestones and Roadmap
 ---
 
-${markdownMilestones}`.replace(/\n\s*\n/g, '\n\n')
+${markdownMilestones}`.replace(/\n\s*\n/g, "\n\n")
 }
 
 const writeMarkdown = (markdown) =>
   new Promise((resolve, reject) => {
     fs.writeFile(outputFile, formatMarkdown(markdown), (err) =>
-      err ? reject(err) : resolve()
+      err ? reject(err) : resolve(),
     )
   })
 
 const formatMarkdown = (markdown) =>
-  prettier.format(markdown, { ...oryStyleOptions, parser: 'markdown' })
+  prettier.format(markdown, { ...oryStyleOptions, parser: "markdown" })
 
 if (orgs.length > 0) {
   renderRepositories(orgs).then(writeMarkdown).catch(console.error)
